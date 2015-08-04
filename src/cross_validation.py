e7408135d3046cdf8b61a8f9583f26e59ca85f49
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-04 13:11:55 -0700

    issues with output fixed

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -9,16 +9,20 @@ cookies = gl.SFrame('../data/cookie_all_basic.csv')
 print
 print 'aggregating'
 aggregated_ip = cookie_ip.groupby(['device_or_cookie_id', 'ip'], {'ip_freq_count': gl.aggregate.MAX('ip_freq_count')})
+print aggregated_ip.shape
 
 print 'merging cookies'
 cookies_new = cookies.join(aggregated_ip, on={'cookie_id': 'device_or_cookie_id'}, how='left')
+print cookies_new.shape
 
 print 'merging train'
 train_new = train.join(aggregated_ip, on={'device_id': 'device_or_cookie_id'}, how='left')
+print train_new.shape
 
 print 'merging cookies and train'
-
 train_cookies = train_new.join(cookies_new, on='ip')
-print train.shape
-print train.column_names()
-print train.head()
+print train_cookies.shape
+print train_cookies.column_names()
+print train_cookies.head()
+
+print sum(train_cookies['drawbridge_handle_x'] == train_cookies['drawbridge_handle_y'])
