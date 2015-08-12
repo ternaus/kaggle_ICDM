b844c242fc1473d3494e62b337bb1e858958b794
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-12 14:58:42 -0700

    Let's try to figure out how does ARGmax work

--- src/cross_validation_train.py
+++ src/cross_validation_train.py
@@ -22,17 +22,29 @@ print 'cookie shape'
 print cookie.shape
 cookie = cookie[cookie['drawbridge_handle'] != '-1']
 print cookie.shape
-#merge cookie with ip
+
+print 'merge cookie with ip'
 cookie_ip = cookie.join(ip, on={'cookie_id': 'device_or_cookie_id'}, how='left')
 
-#merge cookie and train on ip
+print 'merge cookie and train on ip'
+
 cookie_train = train_ip.join(cookie_ip, on=['ip'])
-#count how many drawbridge_handle match
-temp = cookie_train.groupby('device_id', {'drawbridge_hangle': gl.aggregate.SELECT_ONE('drawbridge_handle'),
-                                          'drawbridge_hangle.1': gl.aggregate.SELECT_ONE('drawbridge_handle.1'),
 
-                                          'cookie_id': gl.aggregate.SELECT_ONE('cookie_id')})
-print temp.column_names()
-print temp.shape
+ind = 2
 
-print temp[temp['drawbridge_handle'] == temp['drawbridge_handle.1']].shape
+if ind == 1: #pick random device_id, cookie_id pairs
+  print 'grouping train'
+  temp = cookie_train.groupby('device_id', {'drawbridge_hangle': gl.aggregate.SELECT_ONE('drawbridge_handle'),
+                                            'drawbridge_hangle.1': gl.aggregate.SELECT_ONE('drawbridge_handle.1'),
+                                            'cookie_id': gl.aggregate.SELECT_ONE('cookie_id')})
+  print temp.column_names()
+  print temp.shape
+  print 'count how many drawbridge_handle match'
+  print temp[temp['drawbridge_hangle'] == temp['drawbridge_hangle.1']].shape
+elif ind == 2:#pick device_id, cookie_id pairs when they have maximum count of common IP
+  print 'grouping train'
+  temp = cookie_train.groupby(['device_id', 'drawbridge_hangle', 'drawbridge_hangle.1', 'cookie_id'],
+                              {'count_ip': gl.aggregate.COUNT('ip')})
+  temp_new = temp.groupby(['device_id', 'drawbridge_hangle'], {'max_ip': gl.aggregate.ARGMAX('count_ip', 'drawbridge_hangle.1', 'cookie_id')})
+  print temp_new.head()
+
