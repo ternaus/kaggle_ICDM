804a68c99c7b73dcd79dbb2b592298cd0ac81e2f
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-21 12:55:19 -0700

    Script that will help to do cross validation added.

--- src/create_train_answers.py
+++ src/create_train_answers.py
@@ -24,6 +24,8 @@ print train_cookie.column_names()
 
 train_cookie['cookie_id'] = train_cookie['cookie_id'].apply(lambda x: x + ' ')
 
+print train_cookie['cookie_id'].head()
+
 result = train_cookie.groupby('device_id', {'cookie_id': gl.aggregate.SUM('cookie_id')})
 result['device_id'] = result['device_id'].apply(lambda x: x.strip())
 
