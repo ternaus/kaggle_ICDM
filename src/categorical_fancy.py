c16f8a5a5bad3761b85069262a6f8105bd872fc1
Vladimir Iglovikov <iglovikov@gmail.com>
2015-08-23 16:35:49 -0700

    Let's check simple cross_validation using f1 score

--- src/categorical_fancy.py
+++ src/categorical_fancy.py
@@ -12,20 +12,24 @@ ip = gl.SFrame('../data/id_all_ip.csv')
 print 'Read test'
 test = gl.SFrame('../data/dev_test_basic.csv')
 
-print 'read property category'
-property_category = gl.SFrame('../data/property_category')
+print 'read id_all_property'
+id_all_ip = gl.SFrame('../data/id_all_property.csv')
 
-print 'split property category into device and cookie'
-property_category_device = property_category[property_category['device_or_cookie_indicator'] == 0]
-print property_category_device.shape
-property_category_cookie = property_category[property_category['device_or_cookie_indicator'] == 1]
-print property_category_cookie.shape
+print 'split id_all_ip into device and cookie'
+id_all_ip_cookie = id_all_ip[id_all_ip['device_or_cookie_indicator'] == 1]
+print id_all_ip_cookie.shape
+id_all_ip_device = id_all_ip[id_all_ip['device_or_cookie_indicator'] == 0]
+print id_all_ip_device.shape
+
+print 'read_category'
+category = gl.SFrame('../data/property_category_corrected.csv')
+print category.shape
 
 print 'split ip into device and cookie'
 ip_device = ip[ip['device_or_cookie_id'] == 0]
 print 'ip_device.shape = ', ip_device.shape
 ip_cookie = ip[ip['device_or_cookie_id'] == 1]
-print 'ip_cookie.shape = ', ip_device.shape
+print 'ip_cookie.shape = ', ip_cookie.shape
 
 print 'remove from cookie rows for which draw_bridge_handle = -1'
 print test.shape
@@ -34,12 +38,12 @@ print test.shape
 test = test[test['drawbridge_handle.1'] != -1]
 print test.shape
 
-print 'merge cookie and property'
-cookie_property = cookie.join(property_category_cookie, on = {'cookie_id': 'device_or_cookie_id'})
+print 'merge cookie and id_all_ip'
+cookie_property = cookie.join(id_all_ip_cookie, on = {'cookie_id': 'device_or_cookie_id'})
 print cookie_property.shape
 
-print 'merge device and property'
-device_property = test.join(property_category_device, on = {'device_id': 'device_or_cookie_id'})
+print 'merge device and id_all_ip'
+device_property = test.join(id_all_ip_device, on = {'device_id': 'device_or_cookie_id'})
 print device_property.shape
 
 print 'merge cookie and ip'
@@ -52,6 +56,13 @@ print device_property.shape
 
 print 'merging on ip'
 cookie_test_property = device_property.join(cookie_property, on='ip')
+print cookie_test_property.shape
+
+print 'merge with category'
+cookie_test_property = cookie_test_property.join(category, on='property_id')
+print cookie_test_property.shape
+cookie_test_property = cookie_test_property.join(category, on={'property_id.1': 'property_id'})
+print cookie_test_property.shape
 
 print 'save cookie_test_property to file'
 cookie_test_property.save('../data/cookie_test_property')
